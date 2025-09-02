16b5de9d887b88a073089446500de05f7d122674
prrao87 <prrao87@gmail.com>
2025-09-02 16:34:14 -0400

    Update write algo

--- src/network_analysis/algo_kuzu.py
+++ src/network_analysis/algo_kuzu.py
@@ -20,9 +20,30 @@ conn.execute("CALL project_graph('MentorshipGraph', ['Scholar'], ['MENTORED']);"
 res = conn.execute(
     """
     CALL page_rank('MentorshipGraph')
-    WHERE node.prize = "Physics"
-    RETURN node.name, rank AS pagerank
-    ORDER BY pagerank DESC LIMIT 5
+    RETURN node.name AS name, rank AS pagerank
+    """
+)
+pagerank_df = res.get_as_pl()
+# Update Kuzu database with PageRank metric values
+# First, add a new column pagerank to the Scholar node table
+conn.execute("ALTER TABLE Scholar ADD IF NOT EXISTS pagerank DOUBLE DEFAULT 0.0")
+
+conn.execute(
+    """
+    LOAD FROM pagerank_df
+    MERGE (s:Scholar {name: name})
+    SET s.pagerank = pagerank
+    """
+)
+print("Finished adding graph algorithm metric scores to Kuzu database")
+
+# Query scholars with the highest PageRank scores who won Nobel prizes in Physics 
+res = conn.execute(
+    """
+    MATCH (s:Scholar)
+    WHERE s.prize = "Physics"
+    RETURN s.name, s.pagerank
+    ORDER BY s.pagerank DESC LIMIT 5
     """
 )
 print(res.get_as_pl())
