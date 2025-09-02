2888979dbac5fa7da5ec89a52f1e6d721f278cd7
prrao87 <prrao87@gmail.com>
2025-09-02 17:09:57 -0400

    Add betwennness centrality score for NetworkX graph

--- src/network_analysis/algo_networkx.py
+++ src/network_analysis/algo_networkx.py
@@ -50,3 +50,42 @@ res = conn.execute(
     """
 )
 print(res.get_as_pl())
+
+
+# Run betweenness centrality
+bc_results = nx.betweenness_centrality(nx_graph)
+# NetworkX prefixes the node label to the results
+# This step cleans up the naming so that we can import it back into Kuzu
+betweenness_centrality_df = (
+    pl.DataFrame({"name": k, "metric": v} for k, v in bc_results.items())
+    .with_columns(pl.col("name").str.replace("Scholar_", "").alias("name"))
+)
+
+conn.execute("ALTER TABLE Scholar ADD IF NOT EXISTS betweenness_centrality DOUBLE DEFAULT 0.0")
+
+conn.execute(
+    """
+    LOAD FROM betweenness_centrality_df
+    MERGE (s:Scholar {name: name})
+    SET s.betweenness_centrality = metric
+    """
+)
+print("Finished adding graph algorithm metric scores to Kuzu database")
+
+# Query scholars with the highest PageRank scores who won Nobel prizes in Physics 
+res = conn.execute(
+    """
+    MATCH (s:Scholar),
+          (a:Scholar)-[:MENTORED]->(s)-[:MENTORED]->(b:Scholar)
+    WHERE s.prize = "Physics"
+    AND a.is_laureate = true
+    AND b.is_laureate = true
+    RETURN
+      s.name,
+      s.betweenness_centrality,
+      COUNT(DISTINCT a) AS num_mentors,
+      COUNT(DISTINCT b) AS num_mentees
+    ORDER BY s.betweenness_centrality DESC LIMIT 5
+    """
+)
+print(res.get_as_pl())
